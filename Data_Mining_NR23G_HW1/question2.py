7f556dc92da6c0e18837203144982a8676ef9564
NANDHAKUMAR R G <nandhugr5321@gmail.com>
2024-01-19 17:46:54 -0500

    Update question2.py

--- Data_Mining_NR23G_HW1/question2.py
+++ Data_Mining_NR23G_HW1/question2.py
@@ -1,15 +1,19 @@
 def question2_1():
-    # answer = ['string', 'string', 'string']
+    answer = ['Discrete', 'Quantitative']
     return answer
 
 def question2_2():
+    answer = ['Continuous', 'Quantitative']
     return answer
 
 def question2_3():
+    answer = ['Continuous', 'Quantitative']
     return answer 
 
 def question2_4():
+    answer = ['Discrete', 'Qualitative']
     return answer
 
 def question2_5():
+    answer = ['Nominal', 'Qualitative']
     return answer
