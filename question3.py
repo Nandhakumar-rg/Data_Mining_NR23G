ae1d050deada5a89899bfcca4d55700b7df52cbb
NANDHAKUMAR R G <nandhugr5321@gmail.com>
2024-01-24 16:55:35 -0500

    Commit

--- question3.py
+++ question3.py
@@ -1,31 +1,31 @@
 def question3_1():
-    answer = ['Continuous', 'Quantitative (Ratio)']
+    answer = ['Continuous', 'Ratio']
     return answer
 
 def question3_2():
-    answer = ['Discrete', 'Qualitative (Ordinal)']
+    answer = ['Discrete', ' Ordinal']
     return answer
 
 def question3_3():
-    answer = ['Discrete', 'Qualitative (Nominal)']
+    answer = ['Discrete', 'Nominal']
     return answer 
 
 def question3_4():
-    answer = ['Continuous', 'Quantitative (Ratio)']
+    answer = ['Continuous', 'Ratio']
     return answer
 
 def question3_5():
-    answer = ['Nominal', 'Qualitative (Nominal)']
+    answer = ['Nominal', 'Nominal']
     return answer
 #6,7,8
 def question3_6():
-    answer = ['Continuous', 'Quantitative (Ratio)']
+    answer = ['Continuous', 'Ratio']
     return answer
 
 def question3_7():
-    answer = ['Nominal', 'Qualitative (Nominal)']
+    answer = ['Nominal', 'Nominal']
     return answer
 
 def question3_8():
-    answer = ['Discrete', 'Qualitative (Ordinal)']
+    answer = ['Discrete', 'Ordinal']
     return answer
