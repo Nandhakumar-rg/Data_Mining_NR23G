bfb202aa1375584680baf85253718d8d5a1a22cb
NANDHAKUMAR R G <nandhugr5321@gmail.com>
2024-01-29 20:59:08 -0500

    Update Commit

--- question2.py
+++ question2.py
@@ -1,18 +1,19 @@
 def question2_1():
-    answer = [ 'discrete','quantitative','ratio']
+    answer = [ 'Discrete','Qunatitative', 'Ratio']
     return answer
 
 def question2_2():
-    answer = ['Continuous', 'Quantitative', 'Ratio']
+    answer=['Continuous','Quantitative','Ratio']
     return answer
 
 def question2_3():
-    answer = ['Continuous', 'Quantitative', 'Interval']
+    answer=['Continuous','Quantitative','Ratio']
     return answer 
 
 def question2_4():
-    answer = ['Discrete', 'Qualitative', 'Ordinal']
+    answer=['Discrete','Qualitative','Ordinal']
     return answer
 
 def question2_5():
-    answer = ['Binary', 'Qualitative', 'Nominal']
+    answer=['Binary','Qualitative','Nominal']
+    return answer
